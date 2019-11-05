41a2f5b2ec4870ff7e53a251a0bc122272cbf313
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-05 19:14:43 +0000

    set up controller and index and linked

---
+++ hw_sinatra_cinema/start_point/controller.rb
@@ -0,0 +1,10 @@
+require('sinatra')
+require('sinatra/contrib/all') if development?
+require_relative('./models/film.rb')
+
+also_reload('./models/*')
+
+get '/films' do
+  @film = Film.all()
+  erb(:index)
+end
